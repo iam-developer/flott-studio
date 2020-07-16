72d9ec181c332a807c94a993bf6bc9db1d24cb28
iam-developer <turgunbaaev@gmail.com>
2020-07-17 00:59:10 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,38 @@
+//    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   
+   // Кнопка наверх:
+   $(document).ready(function() {
+    $(window).scroll(function() {
+      if($(this).scrollTop() != 0)
+          $("#toTop").fadeIn();
+      else
+          $("#toTop").fadeOut();
+  });
+  
+      $("#toTop").click(function() {
+          $('body, html').animate({
+              scrollTop:0
+          }, 700);
+      });
+    });
+
+
+  // Burger menu:
+
+  let burgerMenu = document.querySelector('.burger__menu');
+  let leftMenu = document.querySelector('.menu__left');
+  let closeMenu = document.querySelector('.close__menu__btn');
+
+  burgerMenu.onclick = function() {
+    leftMenu.classList.add('open__menu');
+    leftMenu.classList.remove('close__menu');
+    burgerMenu.style.visibility = 'hidden';
+  }
+
+  closeMenu.onclick = function() {
+    leftMenu.classList.add('close__menu');
+    leftMenu.classList.remove('open__menu');
+    burgerMenu.style.visibility = 'visible';
+  }
+
+//    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
